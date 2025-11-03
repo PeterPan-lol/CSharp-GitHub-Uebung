6683a28700eb9c0de5e103d62e3f4e67ce5d97ab
Ruddies <PRuddies@akbk.schule>
2025-11-03 12:30:20 +0100

    Projektdateien hinzufügen.

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        public double Add(double a, double b) { return a + b; }
+        public double Subtract(double a, double b) { return a - b; }
+        public double Multiply(double a, double b) { return a * b; }
+        public double Divide(double a, double b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
